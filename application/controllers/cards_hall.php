940144b88a6e1078b3d59bd8603a3dc44ee9a0f8
Kin Jhon Kiseo <kimkiseo143@gmail.com>
2017-01-21 18:12:36 +0800

    updates(ignore)

--- application/controllers/cards_hall.php
+++ application/controllers/cards_hall.php
@@ -5,10 +5,10 @@ class cards_hall extends CI_Controller {
     public function __construct()
       {
         parent::__construct();
-        $this->load->model('user/user_model','m');
+        // $this->load->model('user/user_model','m');
         $this->load->model('hallModel','hallM');
         $this->load->model('build_m','bm');
-        $this->load->model('user_model','um');
+        // $this->load->model('user_model','um');
         $this->load->model('model_users','mu');
       } 
     public function rigneyHall(){
@@ -22,21 +22,33 @@ class cards_hall extends CI_Controller {
     public function SASavr(){
         $this->load->view('Header/Admin/adminHeader');  
         $this->load->view('Content/calendar/calendar_rigney');
-        $this->load->view('footer/footer');    
+        $this->load->view('footer/footer');
+        $user=$this->session->set_userdata('logged_in')['idNumber'];
+        $this->data['post'] = $this->mu->userfind($user);
+        $this->load->helper('form');     
     }
     public function BCT(){
         $this->load->view('Header/Admin/adminHeader');  
         $this->load->view('Content/calendar/calendar_rigney');
-        $this->load->view('footer/footer');    
+        $this->load->view('footer/footer');
+        $user=$this->session->set_userdata('logged_in')['idNumber'];
+        $this->data['post'] = $this->mu->userfind($user);
+        $this->load->helper('form');     
     }
     public function SAFADtheater(){
         $this->load->view('Header/Admin/adminHeader');  
         $this->load->view('Content/calendar/calendar_rigney');
-        $this->load->view('footer/footer');    
+        $this->load->view('footer/footer');
+        $user=$this->session->set_userdata('logged_in')['idNumber'];
+        $this->data['post'] = $this->mu->userfind($user);
+        $this->load->helper('form');     
     }
     public function MRtheater(){
         $this->load->view('Header/Admin/adminHeader');  
         $this->load->view('Content/calendar/calendar_rigney');
-        $this->load->view('footer/footer');    
+        $this->load->view('footer/footer');
+        $user=$this->session->set_userdata('logged_in')['idNumber'];
+        $this->data['post'] = $this->mu->userfind($user);
+        $this->load->helper('form');     
     }
 }   
